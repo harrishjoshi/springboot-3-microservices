7ac3e6472e1df329fd6889208c5b35949be295f5
harrishjoshi <dev.harishjoshi@gmail.com>
2024-08-11 18:26:39 +0545

    feat: Add create product test

--- product-service/src/test/java/com/harrishjoshi/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/harrishjoshi/microservices/product/ProductServiceApplicationTests.java
@@ -1,13 +1,62 @@
 package com.harrishjoshi.microservices.product;
 
+import com.harrishjoshi.microservices.product.dto.ProductRequest;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
 
-@SpringBootTest
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.7");
 
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        var productRequest = getProductRequest();
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(productRequest)
+                .when()
+                .post("/api/v1/product")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", equalTo(productRequest.name()))
+                .body("description", equalTo(productRequest.description()))
+                .body("skuCode", equalTo(productRequest.skuCode()))
+                .body("price", is(productRequest.price().intValueExact()));
+    }
+
+    private static ProductRequest getProductRequest() {
+        return new ProductRequest(
+                "Product 1", "Product 1 Description", "SKU-1", BigDecimal.valueOf(100)
+        );
+    }
 }
