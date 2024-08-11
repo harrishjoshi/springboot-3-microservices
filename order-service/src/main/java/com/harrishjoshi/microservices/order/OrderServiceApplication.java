a9ed6f06c644c3105f027fe16d68cd290e28ff8e
harrishjoshi <dev.harishjoshi@gmail.com>
2024-08-11 20:12:20 +0545

    feat: Add Feign Client

--- order-service/src/main/java/com/harrishjoshi/microservices/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/harrishjoshi/microservices/order/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package com.harrishjoshi.microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
 
 }
--- order-service/src/main/java/com/harrishjoshi/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/harrishjoshi/microservices/order/service/OrderService.java
@@ -1,8 +1,10 @@
 package com.harrishjoshi.microservices.order.service;
 
+import com.harrishjoshi.microservices.order.config.InventoryClient;
 import com.harrishjoshi.microservices.order.dto.OrderRequest;
 import com.harrishjoshi.microservices.order.model.Order;
 import com.harrishjoshi.microservices.order.repository.OrderRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -10,15 +12,24 @@ import java.util.UUID;
 
 @Service
 @Slf4j
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, InventoryClient inventoryClient) {
         this.orderRepository = orderRepository;
+        this.inventoryClient = inventoryClient;
     }
 
     public void placeOrder(OrderRequest orderRequest) {
+        var isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (!isInStock) {
+            log.error("Product with skuCode {} is out of stock", orderRequest.skuCode());
+            throw new RuntimeException("Product is out of stock");
+        }
+
         var order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         order.setSkuCode(orderRequest.skuCode());
