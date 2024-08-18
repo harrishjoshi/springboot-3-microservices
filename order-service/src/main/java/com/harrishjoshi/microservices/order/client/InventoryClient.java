3790112f9489b1cf9f7a1c58e82e2085cb5fec63
harrishjoshi <dev.harishjoshi@gmail.com>
2024-08-18 16:51:30 +0545

    feat: Add fallback method and retry for order service in stock check

--- order-service/src/main/java/com/harrishjoshi/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/harrishjoshi/microservices/order/client/InventoryClient.java
@@ -1,10 +1,24 @@
 package com.harrishjoshi.microservices.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface InventoryClient {
 
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange("/api/v1/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "inStockFallback")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean inStockFallback(String skuCode, Integer quantity, Throwable throwable) {
+        log.info("Failed to check inventory service for skuCode {} and quantity {}; failure reason: {}", skuCode, quantity,
+                throwable.getMessage());
+        return false;
+    }
 }
